9c12b0994797412818467cebf9c8dc4b77a1c370
spelle <pelle.sebastien@gmail.com>
2013-10-11 13:39:50 +0200

    Eclipse Project files Source

--- AVR_USART_lib.cpp
+++ AVR_USART_lib.cpp
@@ -11,24 +11,47 @@
 
 
 
-void USART_Init( uint16_t ui16UBRR )
+static FILE m_stdout = FDEV_SETUP_STREAM( USART_putchar_printf, NULL, _FDEV_SETUP_WRITE ) ;
+
+
+
+void USART_Init( uint16_t ui16BaudRate )
 {
+	uint32_t ui32UBRR = F_CPU ;
+
+	ui32UBRR >>= 4 ; // (=) to ui32UBRR /= 16 but more efficient
+	ui32UBRR /= ui16BaudRate ;
+	ui32UBRR -= 1 ;
+
 	/* Set baud rate */
-	UBRR0H = (uint8_t) (ui16UBRR>>8) ;
-	UBRR0L = (uint8_t) ui16UBRR ;
+	UBRR0H = (uint8_t) ( (ui32UBRR>>8) & (0x000000FF) ) ;
+	UBRR0L = (uint8_t) ( ui32UBRR & (0x000000FF) ) ;
+
 	/* Enable receiver and transmitter */
 	UCSR0B = (1<<RXEN0) | (1<<TXEN0) ;
+
 	/* Set frame format: 8data, 2stop bit */
 	UCSR0C = (1<<USBS0) | (3<<UCSZ00) ;
+
+	stdout = &m_stdout ;
 }
 
 
 
-void USART_Transmit( uint8_t data )
+void USART_Transmit( uint8_t ui8Data )
 {
 	/* Wait for empty transmit buffer */
 	while ( !( UCSR0A & (1<<UDRE0)) )
 	;
 	/* Put data into buffer, sends the data */
-	UDR0 = data;
+	UDR0 = ui8Data ;
+}
+
+
+
+int USART_putchar_printf( char var, FILE *stream )
+{
+	USART_Transmit( (uint8_t) var ) ;
+
+	return 0 ;
 }
--- AVR_USART_lib.h
+++ AVR_USART_lib.h
@@ -15,13 +15,31 @@
 
 #include <avr/io.h>
 
+#include <stdio.h>
 
 
-void USART_Init( uint16_t ui16UBRR ) ;
+/*! \brief USART Initialization
+ *  \details This function will initialize the USART using selected baudrate.
+ *  		 User must pay attention to select one of the usually used Baud Rate (9600, 19200, 38400, 57600)
+ *  		 Note that since an uint16 is used as argument, Baud rate cannot be more than 57600
+ *  \param [in] uint16_t ui16BaudRate : Desired Baud Rate (16 bit) - up to 57600
+ *  \return Nothing.
+ */
+void USART_Init( uint16_t ui16BaudRate ) ;
 
 
 
-void USART_Transmit( uint8_t data ) ;
+/*! \brief USART Transmit data
+ *  \details Nothing Special. It just wait for the transmit buffer is empty before writing it again.
+ *  \param [in] uint8_t ui8Data : Desired data char to transmit
+ *  \param None.
+ *  \return Nothing.
+ */
+void USART_Transmit( uint8_t ui8Data ) ;
+
+
+
+int USART_putchar_printf( char var, FILE *stream ) ;
 
 
 
